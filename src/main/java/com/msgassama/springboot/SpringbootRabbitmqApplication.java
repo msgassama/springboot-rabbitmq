114e0abb7d42f521e74cd97ec2ccf351b7e31ddd
msgassama <gassamasanoussy15@gmail.com>
2023-01-13 01:54:47 +0000

    setup project

---
+++ src/main/java/com/msgassama/springboot/SpringbootRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.msgassama.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRabbitmqApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/msgassama/springboot/config/RabbitMQConfig.java
@@ -0,0 +1,45 @@
+package com.msgassama.springboot.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author MS GASSAMA
+ */
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.queue.exchange}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    // spring bean for rabbitmq queue
+    @Bean
+    public Queue queue(){
+        return new Queue(queue);
+    }
+
+    // spring bean for rabbitmq exchange
+    @Bean
+    public TopicExchange exchange(){
+        return new TopicExchange(exchange);
+    }
+
+    // binding between queue and exchange using routing key
+    @Bean
+    public Binding binding(){
+        return BindingBuilder.bind(queue())
+                .to(exchange())
+                .with(routingKey);
+    }
+}
