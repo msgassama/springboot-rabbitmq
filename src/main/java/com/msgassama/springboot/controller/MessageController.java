464c11b3b176d796e5f4109a4f991e557cf281b7
msgassama <gassamasanoussy15@gmail.com>
2023-01-15 01:24:03 +0000

    add MessageController to send message and test the producer

---
+++ src/main/java/com/msgassama/springboot/controller/MessageController.java
@@ -0,0 +1,28 @@
+package com.msgassama.springboot.controller;
+
+import com.msgassama.springboot.publisher.RabbitMQProducer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author MS GASSAMA
+ */
+@RestController
+@RequestMapping("/api/v1")
+public class MessageController {
+
+    private final RabbitMQProducer producer;
+
+    public MessageController(RabbitMQProducer producer) {
+        this.producer = producer;
+    }
+
+    @GetMapping("/publish")
+    public ResponseEntity<String> sendMessage(@RequestParam("message") String message){
+        producer.sendMessage(message);
+        return ResponseEntity.ok("Message sent to RabbitMQ ...");
+    }
+}
