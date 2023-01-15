a43f999f2db18bccb60dfeee90f4246d8c076fe5
msgassama <gassamasanoussy15@gmail.com>
2023-01-15 01:40:38 +0000

    add RabbitMQConsumer class

---
+++ src/main/java/com/msgassama/springboot/consumer/RabbitMQConsumer.java
@@ -0,0 +1,19 @@
+package com.msgassama.springboot.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author MS GASSAMA
+ */
+@Service
+public class RabbitMQConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQConsumer.class);
+
+    @RabbitListener(queues = {"${rabbitmq.queue.name}"})
+    public void consume(String message){
+        LOGGER.info("Received message -> {}", message);
+    }
+}
