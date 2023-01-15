97a59673d42fab141c0f390bf7ec493f1ff98309
msgassama <gassamasanoussy15@gmail.com>
2023-01-15 02:04:44 +0000

    configure rabbitmq for json message communication

---
+++ src/main/java/com/msgassama/springboot/dto/User.java
@@ -0,0 +1,13 @@
+package com.msgassama.springboot.dto;
+
+import lombok.Data;
+
+/**
+ * @author MS GASSAMA
+ */
+@Data
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+}
