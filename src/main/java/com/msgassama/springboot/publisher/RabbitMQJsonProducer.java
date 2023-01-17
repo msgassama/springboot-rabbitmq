303ce7f9efc1aae6ab4768733c7f52619be114eb
msgassama <gassamasanoussy15@gmail.com>
2023-01-17 10:05:46 +0000

    add RabbitMQJsonProducer class

---
+++ src/main/java/com/msgassama/springboot/publisher/RabbitMQJsonProducer.java
@@ -0,0 +1,34 @@
+package com.msgassama.springboot.publisher;
+
+import com.msgassama.springboot.dto.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author MS GASSAMA
+ */
+@Service
+public class RabbitMQJsonProducer {
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.json.key}")
+    private String routingJsonKey;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQJsonProducer.class);
+
+    private RabbitTemplate rabbitTemplate;
+
+    public RabbitMQJsonProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendJsonMessage(User user){
+        LOGGER.info("Json message sent -> {}", user);
+        rabbitTemplate.convertAndSend(exchange, routingJsonKey, user);
+    }
+}
